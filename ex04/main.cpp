770fc0d0030aab51e6e8d1d619c6141668ab237c
Hana Kobori <hakobori@c5r1s8.42tokyo.jp>
2025-02-28 20:18:25 +0900

    ex04 deleted unnecessary code

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,26 +6,34 @@ int printError(const char *str)
     return(0);
 }
 
+std::string setOutputFileName(char *argv1)
+{
+    std::string fileName;
+    size_t pos;
+
+    fileName = argv1;
+    pos = fileName.find_last_of(".");
+    if (pos != std::string::npos)
+        fileName = fileName.substr(0, pos);
+    fileName += ".replace";
+    return fileName;
+}
+
 int main(int argc, char *argv[])
 {
     std::string line;
     Replace replace;
+    std::ifstream inputFile(argv[1]);
+    std::string argv2 = argv[2];
+    std::string argv3 = argv[3];
 
     if(argc != 4)
         return (printError("Arguments error"));
-    std::ifstream inputFile(argv[1]);
     if(inputFile.fail())
         return (printError("Input file open error"));
-    std::string outputFileName = argv[1];
-    size_t pos = outputFileName.find_last_of(".");
-    if (pos != std::string::npos)
-        outputFileName = outputFileName.substr(0, pos);
-    outputFileName += ".replace";
-    std::ofstream outputFile(outputFileName.c_str());
+    std::ofstream outputFile(setOutputFileName(argv[1]).c_str());
     if(outputFile.fail())
         return (printError("Output file open error"));
-    std::string argv2 = argv[2];
-    std::string argv3 = argv[3];
     while(std::getline(inputFile, line))
     {
         replace.setNewLine(line, argv2, argv3);
